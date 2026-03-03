b7ed9bdeb4ceb2493a5bbdc55f9d836b214ee7a7
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2026-03-03 10:28:13 +0100

    fix: remove unnecessary xmlns attribute from SVG icons in Pagination component

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -21,7 +21,7 @@ export function Pagination({ currentPage = 1, totalPages = 1, onPageChange }: { 
     return (
         <nav className='pagination'>
             <a className='nav-item' href="" onClick={handlePrevPage}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M15 6l-6 6l6 6" /></svg>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M15 6l-6 6l6 6" /></svg>
             </a>
             {pages.map(page => {
                 return (
@@ -29,7 +29,7 @@ export function Pagination({ currentPage = 1, totalPages = 1, onPageChange }: { 
                 )
             })}
             <a className='nav-item' href="" onClick={handleNextPage}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 6l6 6l-6 6" /></svg>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 6l6 6l-6 6" /></svg>
             </a>
         </nav>
     )
